5182d1966dd69798c6a420ba3389ea3e607fa2e6
Chernokozov Yuriy <Chernokozovyu@gmail.com>
2025-11-20 22:13:14 +0300

    ддобавлен класс контакт и запись 5 ти последних продуктов терминале

--- catalog/models.py
+++ catalog/models.py
@@ -41,5 +41,17 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Email')
+    message = models.TextField(verbose_name='Сообщение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    lastest_products = Product.objects.order_by('-created_at')[:5]
+    print("Последние 5 товаров: ")
+    for product in lastest_products:
+        print(f'- {product.name}: {product.price} руб. (Категория: {product.category.name})')
+    return render(request, "home.html", {'latest_products':lastest_products})
 
 
 def contacts(request):
